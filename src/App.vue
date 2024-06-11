e9691d540a5fb02c0740e21ced7662f09f6a4878
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-11 11:41:39 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import AppBar from './components/AppBar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <v-container fluid>
+        <RouterView />
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
