9c735bae6a55ac7b111683e5a25a45002ba96ab9
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-11 11:43:14 -0300

    ProductMenu

---
+++ src/components/ProductMenu.vue
@@ -0,0 +1,16 @@
+<script setup>
+const categories = await fetch('https://fakestoreapi.com/products/categories').then((res) =>
+  res.json()
+)
+const drawer = 'algo'
+console.log(categories)
+</script>
+<template>
+  <v-navigation-drawer>
+    <v-list-item title="CATEGORIES"></v-list-item>
+    <v-divider></v-divider>
+    <div v-if="categories">
+      <v-list-item v-for="cat in categories" link :key="cat">{{ cat.toUpperCase() }}</v-list-item>
+    </div>
+  </v-navigation-drawer>
+</template>
