51e23f697e5dfb6956c8fe60b98b7cde0f4c72e0
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-11 11:43:25 -0300

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+const links = ['More information', 'About us', 'Legal', 'Partners']
+</script>
+
+<template>
+  <v-footer class="bg-grey-lighten-1">
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        class="mx-2"
+        color="white"
+        rounded="xl"
+        variant="text"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
