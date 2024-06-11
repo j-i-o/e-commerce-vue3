4a93b5cc1884ab0dda43900c02d2b97067feed2f
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-11 11:43:29 -0300

    HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Product from '../components/Product.vue'
+import ProductMenu from '../components/ProductMenu.vue'
+
+const products = await fetch('https://fakestoreapi.com/products?limit=6').then((res) => res.json())
+console.log(products)
+</script>
+
+<template>
+  <v-container>
+    <ProductMenu />
+    <v-row>
+      <v-col cols="4" v-for="prod in products" :key="prod.id">
+        <Product :product="prod" />
+      </v-col>
+    </v-row>
+    <v-row></v-row>
+  </v-container>
+</template>
