4fe01ebe82ac59622de3cd868ca928ca50b87739
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-12 11:30:18 -0300

    Cambios Product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { useCartStore } from '@/stores/cart'
+import { useCategoryStore } from '@/stores/category'
+
+const cart = useCartStore()
+const categories = useCategoryStore()
 const props = defineProps(['product'])
 // type Product = {
 //   id: int;
@@ -16,15 +21,15 @@ const props = defineProps(['product'])
 </script>
 <template>
   <v-card>
-    <v-card-title class="py-5 bg-black text-wrap">
+    <v-card-title class="py-5 text-wrap">
       <v-row>
-        <v-chip size="small">
+        <v-chip size="small" @click="categories.changeCategory(props.product.category)">
           {{ props.product.category.toUpperCase() }}
         </v-chip>
       </v-row>
-      <v-row>
+      <!-- <v-row>
         {{ props.product.title }}
-      </v-row>
+      </v-row> -->
     </v-card-title>
     <v-card-subtitle>
       {{ props.product.description }}
@@ -41,9 +46,21 @@ const props = defineProps(['product'])
     </v-card-text>
 
     <v-card-actions>
-      <b class="mr-2">Price:</b> <span class="bg-yellow rounded"> ${{ props.product.price }}</span>
-      <v-btn icon="mdi-plus"></v-btn>
-      <v-btn icon="mdi-heart"></v-btn>
+      <v-container>
+        <v-row justify="space-around">
+          <span class="bg-yellow rounded-pill px-2"> ${{ props.product.price }}</span>
+          <v-btn
+            icon="mdi-plus"
+            class="bg-grey"
+            density="compact"
+            @click="cart.addProduct(props.product)"
+          ></v-btn>
+        </v-row>
+        <v-row class="mt-0">
+          <v-btn icon="mdi-heart"></v-btn>
+          <v-btn icon="mdi-magnify"></v-btn>
+        </v-row>
+      </v-container>
     </v-card-actions>
   </v-card>
 </template>
