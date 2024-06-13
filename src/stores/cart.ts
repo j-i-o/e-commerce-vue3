32f8bc7360dcaacad49583766896c51b61b94e44
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-06-13 09:20:46 -0300

    Refactoring cartStore

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,45 +1,53 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import { useApiStore } from './api'
+import type { Product } from '@/types/Product'
+import type { ProductWithQuantity } from '@/types/ProductWithQuantity'
 
-interface Product {
-  id: number
-  cant: number
-  price: number
-}
 export const useCartStore = defineStore('cart', () => {
-  const products = ref<Product[]>([])
+  const api = useApiStore()
+  const product = ref<Product | null>(null)
+  const products = ref<ProductWithQuantity[]>([])
+
   const totalCount = computed(() => products.value.length)
   const totalPrice = computed(() =>
     products.value.reduce((total, prod) => total + prod.price * prod.cant, 0)
   )
+  const totalItems = computed(() => products.value.reduce((total, prod) => total + prod.cant, 0))
 
-  function addProduct(product: Omit<Product, 'cant'>): void {
+  async function getProduct(productId: number) {
+    const data = await api.fetchProduct(productId)
+    if (data) {
+      product.value = data
+    }
+  }
+  function updateQuantity(product: Omit<ProductWithQuantity, 'cant'>, cant: number): void {
     const prod = products.value.find((prod) => prod.id === product.id)
     if (prod) {
-      prod.cant += 1
-    } else {
+      if (cant === 0) {
+        products.value = products.value.filter((prod) => prod.id !== product.id)
+      } else {
+        prod.cant = cant
+      }
+    } else if (cant !== 0) {
       products.value.push({ ...product, cant: 1 })
     }
   }
-  function removeProduct(id: number): void {
-    const prod = products.value.find((prod) => prod.id === id)
-    if (prod) {
-      prod.cant--
-    }
-  }
-  function eraseProduct(id: number): void {
-    products.value = products.value.filter((product) => product.id !== id)
+  function getProductQuantity(id: number): number {
+    return products.value.find((prod) => prod.id === id)?.cant ?? 0
   }
   function clearCart() {
     products.value = []
   }
   return {
+    product,
     products,
     totalCount,
     totalPrice,
-    addProduct,
-    removeProduct,
-    eraseProduct,
+    totalItems,
+    getProduct,
+    updateQuantity,
+    getProductQuantity,
     clearCart
   }
 })
