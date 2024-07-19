f03cda1a3c8edc9e9c2e7c56893aa80ee9610724
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-07-19 10:54:49 -0300

    Borrado logs

--- src/stores/loader.ts
+++ src/stores/loader.ts
@@ -5,13 +5,10 @@ export const useLoaderStore = defineStore('loader', () => {
   const loading = ref(false)
 
   function setLoader() {
-    console.log('seteando loader')
-
     loading.value = true
   }
 
   function removeLoader() {
-    console.log('quitando loader')
     loading.value = false
   }
 
