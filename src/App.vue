66adabb959012ebe356e6583d6566910a47847e5
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-07-19 10:53:23 -0300

    Implementación Loader

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import { RouterView } from 'vue-router'
 import AppBar from './components/AppBar.vue'
 import Footer from './components/Footer.vue'
 import CategoryDrawer from './components/CategoryDrawer.vue'
+import Loader from './components/Loader.vue'
 </script>
 
 <template>
   <v-app>
+    <Loader />
     <CategoryDrawer />
     <AppBar />
     <Footer />
