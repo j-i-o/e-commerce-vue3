46368b9e6c5edb74b645d25f8a559b4036579607
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-07-19 14:24:02 -0300

    IntroPopup y FavDrawer

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@ import AppBar from './components/AppBar.vue'
 import Footer from './components/Footer.vue'
 import CategoryDrawer from './components/CategoryDrawer.vue'
 import Loader from './components/Loader.vue'
+import IntroPopup from './components/IntroPopup.vue'
+import FavDrawer from './components/FavDrawer.vue'
 </script>
 
 <template>
   <v-app>
+    <IntroPopup />
     <Loader />
     <CategoryDrawer />
+    <FavDrawer />
     <AppBar />
     <Footer />
     <v-main>
