e7b453833bc1ece76adf84b8e6f769a96f049e8f
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-07-19 14:23:20 -0300

    Chip de categoría seleccionada

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
 import { onMounted, computed } from 'vue'
 import Product from '@/components/Product.vue'
 import Carousel from '@/components/Carousel.vue'
-// import ProductMenu from '@/components/ProductMenu.vue'
 import { useHomeStore } from '@/stores/home'
 import { useLoaderStore } from '@/stores/loader'
 
@@ -13,13 +12,11 @@ const products = computed(() => homeStore.products)
 
 onMounted(async () => {
   await homeStore.getProducts()
-  console.log(products)
 })
 </script>
 
 <template>
   <v-container>
-    <!-- <ProductMenu /> -->
     <v-row v-if="!loading && products.length" justify="center">
       <v-col cols="12" md="6">
         <Carousel :products="products" />
@@ -29,6 +26,16 @@ onMounted(async () => {
       {{ loading }}
     </v-row>
     <v-row v-else>
+      <v-col v-if="!!homeStore.currentCategory" cols="12">
+        <v-chip class="bg-grey-lighten-2 text-black px-2" size="lg">
+          {{ homeStore.currentCategory.toUpperCase() }}
+          <v-icon
+            color="red-lighten-1"
+            icon="mdi-close"
+            @click.prevent="homeStore.getProducts()"
+          ></v-icon>
+        </v-chip>
+      </v-col>
       <v-col cols="6" md="4" lg="3" v-for="prod in products" :key="prod.id">
         <Product :product="prod" />
       </v-col>
