2bf989b29fea03199b452779097a5880cb3b596d
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2024-07-22 11:20:34 -0300

    Limpiar producto al saltar a otro

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
 import CartView from '../views/CartView.vue'
+import { useCartStore } from '@/stores/cart'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,13 @@ const router = createRouter({
       component: CartView
     }
   ]
+})
+
+router.beforeEach((to) => {
+  const cartStore = useCartStore()
+  if (to.name === 'product') {
+    cartStore.product = null
+  }
 })
 
 export default router
